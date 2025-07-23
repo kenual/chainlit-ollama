81cad8f81b0d24c1a9cbc24aeba4e3a47f218a50
kenual <klau>
2025-07-22 21:47:32 -0700

    Remove duckduckgo models

--- src/llm_service.py
+++ src/llm_service.py
@@ -11,12 +11,6 @@ from agent_helper import OLLAMA_API_BASE, agent_runner
 logger = logging.getLogger(__name__)
 
 SERVICE_MODELS = [
-    {'name': "claude-3-haiku", 'model': "Cloud Service: claude-3-haiku-20240307"},
-    {'name': "gpt-4o-mini", 'model': "Cloud Service: gpt-4o-mini"},
-    {'name': "o3-mini", 'model': "Cloud Service: o3-mini"},
-    {'name': "llama-3.3-70b",
-        'model': "Cloud Service: meta-llama/Llama-3.3-70B-Instruct-Turbo"},
-    {'name': "mixtral-8x7b", 'model': "Cloud Service: mistralai/Mixtral-8x7B-Instruct-v0.1"}
 ]
 if os.getenv('COHERE_API_KEY'):
     SERVICE_MODELS.insert(
@@ -47,9 +41,6 @@ async def chat_messages_send_response(model: str, messages: List[Dict[str, str]]
     if 'Cloud Service: ' in model:
         if 'command-r-plus' in model or "together_ai" in model:
             litellm_model = model.split("Cloud Service: ")[1]
-        else:
-            await service_chat_messages_send_response(model=model, messages=messages)
-            return
     
     # Send chat messages to Ollama and stream the response back to the client.
     translation_table = str.maketrans({'.': '_', ':': '#'})
@@ -115,52 +106,3 @@ async def chat_messages_send_response(model: str, messages: List[Dict[str, str]]
                 await think_step.stream_token(token)
             else:
                 await assistant_response.stream_token(token)
-
-
-def get_continuation_headers(response: httpx.Response) -> dict:
-    return {SERVICE_CHAT_CONTINUATION_HEADER_KEY:
-            response.headers.get(SERVICE_CHAT_CONTINUATION_HEADER_KEY, '')}
-
-
-async def service_chat_messages_send_response(model: str, messages: List[Dict[str, str]]) -> None:
-    http_timeout = 15
-    model = model.rsplit(sep=' ', maxsplit=1)[1] # remove cloud service prefix
-
-    translation_table = str.maketrans({'.': '_', ':': '#'})
-    assistant_response = cl.Message(
-        content='', author=model.translate(translation_table))
-
-    headers = cl.user_session.get(CHAT_SESSION_HEADERS)
-    logging.info(f"Use continuation headers: {headers}")
-    if headers is None:
-        response = httpx.get(url='https://duckduckgo.com/duckchat/v1/status',
-                             headers={'x-vqd-accept': '1'},
-                             timeout=http_timeout
-                             )
-        headers = get_continuation_headers(response)
-
-    client = httpx.AsyncClient()
-    part = {
-        "model": model,
-        "messages": messages
-    }
-    async with client.stream(method='POST',
-                             url='https://duckduckgo.com/duckchat/v1/chat',
-                             headers=headers,
-                             json=part,
-                             timeout=http_timeout
-                             ) as response:
-        async for line in response.aiter_lines():
-            if line.startswith("data: "):
-                data = line[5:].strip()
-                if not '[DONE]' in data:
-                    part = json.loads(data)
-                    if 'message' in part:
-                        await assistant_response.stream_token(part['message'])
-                else:
-                    await assistant_response.send()
-                    logger.info(data)
-
-        headers = get_continuation_headers(response)
-        cl.user_session.set(CHAT_SESSION_HEADERS, headers)
-        logging.info(f"Set continuation headers: {headers}")
